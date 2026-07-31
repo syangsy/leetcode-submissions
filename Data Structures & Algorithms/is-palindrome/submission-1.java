02518e141f89b769b9a8749313b3b80cb2137b7c
Sarah Yang <sy.sarahyang.sy@gmail.com>
2026-07-31 11:11:29 -0400

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+
+        s = s.toLowerCase().replaceAll("\\s", "");
+        s= s.replaceAll("[^a-zA-Z0-9]", "");
+ 
+        int start = 0;
+        int end = s.length()-1;
+
+        while(start <= end){
+            System.out.println(s.charAt(start));
+            System.out.println(s.charAt(end));
+            if(s.charAt(start) != s.charAt(end)){
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+        return true;  
+    }
+}
