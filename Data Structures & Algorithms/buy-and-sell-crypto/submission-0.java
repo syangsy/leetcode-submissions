a2893d4091ee1713c6c262b0c5bb4a1e6c256844
Sarah Yang <sy.sarahyang.sy@gmail.com>
2026-09-02 15:26:29 -0400

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+
+        int maxProfit = 0;
+        int min = Integer.MAX_VALUE;
+
+        for(int i = 0; i < prices.length; i++){
+
+            if(prices[i] < min)
+                min = prices[i];
+            
+            int profit = prices[i] - min;
+
+            if(profit > maxProfit)
+                maxProfit = profit;
+
+        }
+        
+        return maxProfit;
+        
+    }
+}
